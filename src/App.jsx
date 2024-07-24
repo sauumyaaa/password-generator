cca37708cd5c8ef02fbc0ce33a019fa5afcc5721
saumyahere <sauumyaaa@gmail.com>
2024-07-25 04:14:38 +0530

    update: styling

--- src/App.jsx
+++ src/App.jsx
@@ -48,27 +48,34 @@ function App() {
   //take password as dependency and see the infinite loop
 
   return (
-    <>
+    <div
+        className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat "
+        style={{
+            backgroundImage: `url('https://images.unsplash.com/photo-1547190027-9156686aa2f0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`,
+        }}
+    >
 
-    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-700'>
-    <h1 className='text-lg text-center text-white my-3'>PASSWORD GENERATOR</h1>
+    <div className='w-full max-w-xl mx-auto shadow-md rounded-lg px-4 py-1 my-8 text-white '>
+    <h1 className='text-3xl text-center text-white my-3 mb-20'>
+    Strong passwords instantly!
+    </h1>
     <div className='flex shadow-md rounded-lg overflow-hidden mb-4'>
       <input
       type='text'
       value={password}
-      className='outline-none w-full py-1 px-3'
+      className='outline-none w-full py-1 px-3 text-green-800 text-lg'
       placeholder='password'
       readOnly
       ref={passwordRef}
       ></input>
       <button
       onClick={copyPasswordToClipboard}
-      className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
+      className='outline-none bg-green-800 text-white px-3 py-0.5 shrink-0 text-lg'
       >copy</button>
 
     </div>
-    <div className='flex text-sm gap-x-2'>
-      <div className='flex items-center gap-x-1'>
+    <div className='flex text-sm gap-x-4 '>
+      <div className='flex items-center gap-x-1 '>
         <input type="range" 
         min={6}
         max={30}
@@ -107,7 +114,7 @@ function App() {
     </div>
     </div>
    
-    </>
+    </div>
   )
 }
 
